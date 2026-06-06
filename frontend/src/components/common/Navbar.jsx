4136acd00e65291e9431b6931302b50f977a4dc5
MANTHAN <manthansingh2601@gmail.com>
2026-06-06 15:35:21 +0530

    Refactor Navbar layout for improved responsiveness

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -101,7 +101,7 @@ export default function Navbar() {
 
   return (
     <nav className="bg-white border-b border-gray-200 sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           {/* Logo */}
           <div className="flex items-center">
